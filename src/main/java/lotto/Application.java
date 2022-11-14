4def7bdf82fe63bc480d6d3c57554ed5e3edcbfa
Luka <meena2003@naver.com>
2022-11-15 01:14:13 +0900

    feat(lotto): skeleton 코드 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,51 @@
 package lotto;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+    }
+
+    private List<Lotto> buyLottery(int count) {
+
+        return null;
+    }
+
+    private Integer compareNumbers(int n) {
+
+        return null;
+    }
+
+    private float calculateRateOfReturn() {
+
+        return 0f;
+    }
+
+    private Integer askHowMuchToBuy() {
+
+        return 0;
+    }
+
+    private List<Integer> askWinningNumber() {
+
+        return null;
+    }
+
+    private Integer askBonusNumber() {
+
+        return null;
+    }
+
+    private void printLotteryCountAndNumbers() {
+
+    }
+
+    private void printLotteryWinningResult() {
+
+    }
+
+    private void printRateOfReturn() {
+
     }
 }
---
+++ src/main/java/lotto/Numbers.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import java.util.List;
+
+public class Numbers {
+    public List<Integer> createRandomNumbers() {
+
+        return null;
+    }
+
+    public List<Integer> sortNumbers() {
+
+        return null;
+    }
+
+    public void validate() {
+
+    }
+}
